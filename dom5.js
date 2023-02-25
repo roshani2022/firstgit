db724420e425bf97950f281e529419ce9a76cccb
roshani2022 <rosh.gupta17@gmail.com>
2023-02-25 21:37:25 +0530

    git add fifth file

---
+++ dom5.js
@@ -0,0 +1,20 @@
+//var items = document.getElementsByClassName('list-group-item')
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent = "Hello";
+//items[2].fontWeight='bold';
+//items[2].style.backgroundColor="green";
+//items[1].style.backgroundColor='yellow'
+//for(var i=0;i<items.length;i++){
+//    items[i].style.backgroundColor='#f4f4f4';
+//}
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "Hello";
+li[2].fontWeight='bold';
+li[2].style.backgroundColor="green";
+li[1].style.backgroundColor='yellow'
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+}
